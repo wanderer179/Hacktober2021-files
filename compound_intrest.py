d969f9a2cc1cff2ec0675a582dc45e619c5bfe83
vivek-2567 <77964526+vivek-2567@users.noreply.github.com>
2021-10-12 17:32:12 +0530

    Delete compound_intrest.py

--- compound_intrest.py
+++
@@ -1,7 +0,0 @@
-p = int(input("Enter the Principle amount:- "))
-r = float(input("Enter the rate of Intrest:- "))
-t = float(input("Enter the time period:- "))
-
-ans = p*((1+(r/100))**(t))
-print("\n Your Compound Intrest is:- ")
-print(ans-p,'\n')
